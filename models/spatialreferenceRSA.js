7e7a926d3971046800f2f1d61bf20a78beed455c
Mathieu Rolfo <rolfom01@gmail.com>
2016-11-15 13:51:11 -0800

    Segmented spatial RSA into own file

---
+++ models/spatialreferenceRSA.js
@@ -0,0 +1,20 @@
+// LITERAL LISTENER MODELS
+
+// assume P(s) is uniform
+
+// TODO: get the form of the input data, to determine how to process a literal "in"
+var literalIn = function(lily, utterance, world) {
+	return true;
+}
+
+var literalNear = function(lily, utterance) {
+	return true;
+}
+
+
+// INFORMATIVE SPEAKER MODELS
+
+// assume P(u) is uniform
+
+
+// PRAGMATIC LISTENER MODELS
